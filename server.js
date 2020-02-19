1862feb44c94ddacdb6ff40b4f4c5d36f0524b4a
ismail2803 <ismail.a2803@gmail.com>
2020-02-19 11:41:55 +0700

    perbaiki no absen

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ var server = http.createServer(function (req, res) {
     res.end("Hi, Nama saya Ismail");
 });
 
-server.listen(8040);
+server.listen(8030);
 
-console.log("server running on http://localhost:8040");
+console.log("server running on http://localhost:8030");
